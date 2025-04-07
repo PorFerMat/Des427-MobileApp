53157d36dd9ef049f4945b8c6beedfd292d1ff62
PorFerMat <189195118+PorFerMat@users.noreply.github.com>
2025-04-07 19:22:10 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+//import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
