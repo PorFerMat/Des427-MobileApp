f74106e7df993301ea0222af0a51b501594d5f60
Chonmanee Chimpad <chonmanee.chi@gmail.com>
2025-05-06 19:15:53 +0700

    update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,7 +3,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { firebaseConfig } from './firebaseConfig'; // Add proper import path
+// import { firebaseConfig } from './firebaseConfig'; // Add proper import path
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBi4YLq8vah40AxuUgTw1pJdRm2rlj1Q8Y",
+    authDomain: "des427-8f9bb.firebaseapp.com",
+    projectId: "des427-8f9bb",
+    storageBucket: "des427-8f9bb.firebasestorage.app",
+    messagingSenderId: "56584507771",
+    appId: "1:56584507771:web:65577d2b650e684ea16dde",
+    measurementId: "G-BFHVZZZEL5"
+};
 
 const app = initializeApp(firebaseConfig);
 
