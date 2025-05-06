7deda62e920b2cc14b9dabb92c1d54a75903b33c
Chonmanee Chimpad <chonmanee.chi@gmail.com>
2025-05-06 22:47:29 +0700

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,25 +2,32 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { auth } from '@/services/firebase';
 
-const AuthContext = createContext<{ currentUser: User | null }>({ currentUser: null });
+// const AuthContext = createContext<{ currentUser: User | null }>({ currentUser: null });
 
 interface AuthContextType {
   currentUser: User | null;
+  loading: boolean;
 }
 
+const AuthContext = createContext<AuthContextType>({
+  currentUser: null,
+  loading: true,
+});
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, user => {
       setCurrentUser(user);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ currentUser, loading }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => useContext(AuthContext);
